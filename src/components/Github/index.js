f1129de7f7b7bfb9585b71beb3c5f83368651415
aurelia Segarra <av.segarra@gmail.com>
2021-02-04 12:35:28 +0100

    connection with the Github API ok

--- src/components/Github/index.js
+++ src/components/Github/index.js
@@ -1,5 +1,6 @@
 // Import npm
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 // Import components
 import SearchBar from 'src/components/SearchBar';
@@ -7,7 +8,6 @@ import Message from 'src/components/Message';
 import Repos from 'src/components/Repos';
 
 // Import data
-import reposData from 'src/data/repos';
 import logoGithub from './logo-github.png';
 import './style.scss';
 
@@ -24,11 +24,34 @@ const Github = () => {
     description: repo.description ? repo.description : 'Pas de description',
   }));
 
-  const [repos, setRepos] = useState(repoParser(reposData.items));
+  const [repos, setRepos] = useState([]);
+  const [inputValue, setInputValue] = useState('');
+  const [searchQuery, setSearchQuery] = useState(''); // to store the data for submission
+
+  const baseUrl = `https://api.github.com/search/repositories?q=${searchQuery}`;
+
+  useEffect(async () => {
+    if (searchQuery) {
+      try {
+        const response = await axios.get(baseUrl);
+        const items = repoParser(response.data.items);
+        console.log(items);
+        setRepos(items);
+      }
+      catch (error) {
+        console.log(error);
+      }
+    }
+  }, [searchQuery]);
+
   return (
     <div className="git">
       <img className="logo" src={logoGithub} alt="github logo" />
-      <SearchBar />
+      <SearchBar
+        inputValue={inputValue}
+        onChangeInputValue={setInputValue}
+        onSubmitForm={setSearchQuery}
+      />
       <Message message="la recherche a trouvé X résultats" />
       <Repos repos={repos} />
     </div>
--- src/components/SearchBar/index.js
+++ src/components/SearchBar/index.js
@@ -1,22 +1,42 @@
 // Import npm
 import React from 'react';
+import PropTypes from 'prop-types';
 
 // Import data
 import './style.scss';
 
 // Component
-const SearchBar = () => (
-  <form className="container">
-    <label className="search-label" htmlFor="search-input">
-      <input
-        type="text"
-        id="search-input"
-        placeholder="Search ..."
-      />
-      <i className="fa fa-search search-icon" />
-    </label>
-  </form>
-);
+const SearchBar = ({ inputValue, onChangeInputValue, onSubmitForm }) => {
+  const handleOnChange = (event) => {
+    onChangeInputValue(event.target.value);
+  };
+
+  const handleOnSubmit = (event) => {
+    event.preventDefault();
+    onSubmitForm(inputValue);
+  };
+
+  return (
+    <form className="container" onSubmit={handleOnSubmit}>
+      <label className="search-label" htmlFor="search-input">
+        <input
+          type="text"
+          id="search-input"
+          value={inputValue}
+          onChange={handleOnChange}
+          placeholder="Search ..."
+        />
+        <i className="fa fa-search search-icon" />
+      </label>
+    </form>
+  );
+};
+
+SearchBar.propTypes = {
+  inputValue: PropTypes.string.isRequired,
+  onChangeInputValue: PropTypes.func.isRequired,
+  onSubmitForm: PropTypes.func.isRequired,
+};
 
 // Export
 export default SearchBar;
